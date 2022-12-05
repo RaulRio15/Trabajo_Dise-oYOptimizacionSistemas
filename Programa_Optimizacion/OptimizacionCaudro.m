5b6a9096ed0cae896d37df05b12a3e3504ea5efa
RaulRio15 <119752563+RaulRio15@users.noreply.github.com>
2022-12-05 20:41:16 +0100

    Subida Raul 05/12/2022
    Modifico el excel introduciendo las conexiones y la posición de la toma al exterior
    En el programa principal se leen del excel las posiciones de la toma exterior y las conexiones
    Añado el programa del calculo de las bornas de Gonzalo

--- Programa_Optimizacion/OptimizacionCaudro.m
+++ Programa_Optimizacion/OptimizacionCaudro.m
@@ -3,13 +3,15 @@ clear
 close all
 
 %Leemos los datos que se introducen en la excell
-DimCuadro = xlsread('Inforrmacion_Cuadro.xlsx',1,'A3:B3'); %Vector 1 Fila 2 Columnas dimensiones cuadro Columna1=Ancho y  Columna2=Alto
-DimComp = xlsread('Inforrmacion_Cuadro.xlsx',1,'D3:G13'); %Matriz con las dimensiones de los componenes 
+DimCuadro = xlsread('Informacion_Cuadro.xlsx',1,'A:B'); %Vector 1 Fila 2 Columnas dimensiones cuadro Columna1=Ancho y  Columna2=Alto
+DimComp = xlsread('Informacion_Cuadro.xlsx',1,'D:G'); %Matriz con las dimensiones de los componenes 
+PosExt = xlsread('Informacion_Cuadro.xlsx',1,'I:J'); %Vector 1 Fila 2 Columnas coordenadas toma exterior Columna1=X y  Columna2=Y
+Conexiones = xlsread('Informacion_Cuadro.xlsx',2,'A:F'); %Matriz con las dimensiones de los componenes 
 
-Canaleta = 20; %Indicar la dimensison de la canaleta
+Canaleta = 40; %Indicar la dimensison de la canaleta
 
 %%
-Orden = [1 2 3 4 5 6 7 8 9 10 11];
+Orden = [14 13 12 11 10 9 8 7 6 5 4 3 2 1];
 
 [CompOrd] = OrdenarComponentes(Orden, DimComp) %Función que ordena los componentes con el orden de introducir que se haya indicado
 [PosComp, Carril] = CalculoPosicionComponentes(DimCuadro, CompOrd, Canaleta) %Función que calcula la posición de los componentes según el orden que se ha indicado
